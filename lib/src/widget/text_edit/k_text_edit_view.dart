9fbd644e4eb2800fafce3c87280e1383089c78fb
shivamsharma <shivams1110@live.in>
2023-05-17 16:00:30 +0530

    revert change  REgex to string as parameter

--- lib/src/widget/text_edit/k_text_edit_view.dart
+++ lib/src/widget/text_edit/k_text_edit_view.dart
@@ -10,7 +10,7 @@ class KTextEditView extends StatefulWidget {
   final TextInputAction? textInputAction;
   final bool isValidation;
   final String? errorMessage;
-  final RegExp? validateRegex;
+  final String? validateRegex;
   final InputDecoration? inputDecoration;
   final bool isEnabled;
 
@@ -59,7 +59,7 @@ class _KTextEditViewState extends State<KTextEditView> {
         controller: widget.controller,
         validator: (value) {
           if (value != null) {
-            final regex = widget.validateRegex!;
+            final regex = RegExp(widget.validateRegex!);
             if (!regex.hasMatch(value)) {
               return widget.errorMessage ?? "";
             }
