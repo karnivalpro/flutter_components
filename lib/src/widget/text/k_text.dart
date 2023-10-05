6e955493317e87c8c6c7a140940fadb12a7c01b7
shivamsharma <shivams1110@live.in>
2023-10-05 09:42:34 +0530

    added selection Text for web

--- lib/src/widget/text/k_text.dart
+++ lib/src/widget/text/k_text.dart
@@ -1,23 +1,36 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_component/flutter_component.dart';
 import 'package:flutter_component/src/constants/string_constants.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class KText extends StatelessWidget {
   final String text;
   final TextStyle? textStyle;
   final TextAlign? textAlign;
+  final bool isSelectionEnable;
   final TextOverflow? textOverflow;
 
   const KText(
       {Key? key,
       required this.text, this.textStyle,
         this.textAlign,
+        this.isSelectionEnable = false,
       this.textOverflow})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(
+    return kIsWeb && isSelectionEnable ? SelectableText(text,
+      textAlign: textAlign ?? TextAlign.start,
+      // overflow: textOverflow ?? TextOverflow.clip,
+      style: textStyle ?? KTextStyles.textStyle(
+          fontSize: 12.0,
+          fontFamily: Constants.inter,
+          fontWeight: FontWeight.normal,
+          decoration: TextDecoration.none,
+          letterSpacing: 0.1,
+          fontColor: Colors.black),) : Text(
       text,
       textAlign: textAlign ?? TextAlign.start,
       overflow: textOverflow ?? TextOverflow.clip,
